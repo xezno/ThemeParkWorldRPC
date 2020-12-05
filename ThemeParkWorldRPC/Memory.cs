63cca2e02686960f1b5eb3b7c1fa3206ba048dd0
Alex <alexnoir03@gmail.com>
2020-12-05 00:52:36 +0000

    Initial commit

---
+++ ThemeParkWorldRPC/Memory.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ThemeParkRPC
+{
+    // Class adapted from https://stackoverflow.com/a/50672487
+    public class Memory
+    {
+        private static Process process;
+        private static IntPtr processHandle;
+
+        private static int bytesWritten;
+        private static int bytesRead;
+
+        public static bool Attach(string processName)
+        {
+            if (Process.GetProcessesByName(processName).Length > 0)
+            {
+                process = Process.GetProcessesByName(processName)[0];
+                processHandle =
+                    OpenProcess(Flags.PROCESS_VM_OPERATION | Flags.PROCESS_VM_READ | Flags.PROCESS_VM_WRITE,
+                        false, process.Id);
+                return true;
+            }
+
+            return false;
+        }
+
+        public static T ReadMemory<T>(int address) where T : struct
+        {
+            var ByteSize = Marshal.SizeOf(typeof(T));
+
+            var buffer = new byte[ByteSize];
+
+            ReadProcessMemory((int)processHandle, address, buffer, buffer.Length, ref bytesRead);
+
+            return ByteArrayToStructure<T>(buffer);
+        }
+
+        public static byte[] ReadMemory(int offset, int size)
+        {
+            var buffer = new byte[size];
+
+            ReadProcessMemory((int)processHandle, offset, buffer, size, ref bytesRead);
+
+            return buffer;
+        }
+
+        public static string ReadMemoryString(int address, int size)
+        {
+            return Encoding.ASCII.GetString(ReadMemory(address, size));
+        }
+
+        public static float[] ReadMatrix<T>(int Adress, int MatrixSize) where T : struct
+        {
+            var ByteSize = Marshal.SizeOf(typeof(T));
+            var buffer = new byte[ByteSize * MatrixSize];
+            ReadProcessMemory((int)processHandle, Adress, buffer, buffer.Length, ref bytesRead);
+
+            return ConvertToFloatArray(buffer);
+        }
+
+        public static int GetModuleAddress(string Name)
+        {
+            try
+            {
+                foreach (ProcessModule ProcMod in process.Modules)
+                    if (Name == ProcMod.ModuleName)
+                        return (int)ProcMod.BaseAddress;
+            }
+            catch
+            {
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ERROR: Cannot find - " + Name + " | Check file extension.");
+            Console.ResetColor();
+
+            return -1;
+        }
+
+        #region P/Invoke
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+        #endregion
+
+        #region Other
+
+        internal struct Flags
+        {
+            public const int PROCESS_VM_OPERATION = 0x0008;
+            public const int PROCESS_VM_READ = 0x0010;
+            public const int PROCESS_VM_WRITE = 0x0020;
+        }
+
+        #endregion
+
+        #region Conversion
+
+        public static float[] ConvertToFloatArray(byte[] bytes)
+        {
+            if (bytes.Length % 4 != 0)
+                throw new ArgumentException();
+
+            var floats = new float[bytes.Length / 4];
+
+            for (var i = 0; i < floats.Length; i++)
+                floats[i] = BitConverter.ToSingle(bytes, i * 4);
+
+            return floats;
+        }
+
+        private static T ByteArrayToStructure<T>(byte[] bytes) where T : struct
+        {
+            var handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            try
+            {
+                return (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+            }
+            finally
+            {
+                handle.Free();
+            }
+        }
+
+        private static byte[] StructureToByteArray(object obj)
+        {
+            var length = Marshal.SizeOf(obj);
+
+            var array = new byte[length];
+
+            var pointer = Marshal.AllocHGlobal(length);
+
+            Marshal.StructureToPtr(obj, pointer, true);
+            Marshal.Copy(pointer, array, 0, length);
+            Marshal.FreeHGlobal(pointer);
+
+            return array;
+        }
+
+        #endregion
+    }
+}
---
+++ ThemeParkWorldRPC/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading;
+
+namespace ThemeParkRPC
+{
+    class Program
+    {
+        static Discord.Discord discord;
+        static long startTime;
+
+        static void Main(string[] args)
+        {
+            /*
+             * Addresses:
+             * +007CB2DC: Int32 - Cash
+             * +00871F74: String - level name (jungle, hallow, fantasy, space)
+             * +0087505C: Ditto
+             * +00876668: Ditto
+             * +00FAA40D: Ditto
+             * +00768F3D / 05C4D745?: "lobby" or "level", depending on whether the player is in lobby or not
+             */
+
+            discord = new Discord.Discord(784571542221750283, (ulong)Discord.CreateFlags.Default);
+
+            Console.WriteLine("Looking for tp.exe..."); 
+
+            var found = false;
+            while (!found)
+            {
+                Thread.Sleep(250);
+                found = Memory.Attach("tp_patched");
+            }
+
+            startTime = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+            while (true)
+            {
+                var cash = Memory.ReadMemory<int>(0x007CB2DC);
+                var level = Memory.ReadMemoryString(0x00871F74, 6);
+
+                var lobbyStr = Memory.ReadMemoryString(0x05CCD745, 5);
+                Console.WriteLine(lobbyStr);
+                var inLobby = lobbyStr != "level";
+                UpdateActivity(cash, inLobby, level);
+                discord.RunCallbacks();
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static string PrettifyLevelName(string levelName)
+        {
+            switch (levelName)
+            {
+                case "jungle":
+                    return "Lost Kingdom";
+                case "space\\":
+                    return "Space Zone";
+                case "hallow":
+                    return "Halloween World";
+                case "fantas":
+                    return "Wonder Land";
+                default:
+                    return levelName;
+            }
+        }
+
+        private static void UpdateActivity(int cash, bool inLobby, string level)
+        {
+            discord.GetActivityManager().UpdateActivity(new Discord.Activity()
+            {
+                State = (inLobby) ? "In lobby" : PrettifyLevelName(level),
+                Details = inLobby ? null : $"${cash}",
+                Assets = new Discord.ActivityAssets()
+                {
+                    LargeImage = "tpw"
+                },
+                Timestamps = new Discord.ActivityTimestamps()
+                {
+                    Start = startTime
+                }
+            }, (res) =>
+            {
+                if (res == Discord.Result.Ok)
+                    Console.WriteLine("Set activity.");
+                else
+                    Console.WriteLine($"Set activity failed: {res}");
+            });
+        }
+    }
+}
